9eb98208ab584630977163061326dd1537c89418
jephthah <jephthah.mbah@outlook.com>
2023-11-07 10:03:55 +0100

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Price Scrapping  App",
+  description: "Track prices of products",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="max-w-10xl mx-auto">
+          <Navbar />
+
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
