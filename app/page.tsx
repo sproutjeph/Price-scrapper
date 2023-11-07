5c29b23c425c7ab42c4f7831a8a97c611c4a2e7f
jephthah <jephthah.mbah@outlook.com>
2023-11-07 10:55:46 +0100

    hero carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import HeroCarousel from "@/components/HeroCarousel";
 import Searchbar from "@/components/Searchbar";
 import Image from "next/image";
 
@@ -30,7 +31,7 @@ export default function Home() {
             <Searchbar />
           </div>
 
-          {/* <HeroCarousel /> */}
+          <HeroCarousel />
         </div>
       </section>
 
